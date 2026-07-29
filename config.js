f9c8d37d80ce31a8cf50752af61362cf5367f560
hjonkers <59563470+hjonkers@users.noreply.github.com>
2026-07-29 10:29:59 +0200

    Update config.js
    dashboard link to csv updated

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
    ============================================================ */
 
 const MATHSPHERE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxLd1BlHW5Mb-emQ9_tMsTcB-U2Gji1Iu30ZASuV7g_Duv7tDF2D1pFpSzmgx7YtxMK0w/exec";
-const MATHSPHERE_DASHBOARD_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQH5a2TAZk-5N2Z-EK0ICk--y6lNA3wk-3GnvHTPND_IGmVwKABZmgtk07LH6K4RMsa-P7QTcHA_U3W/pub?gid=0&single=true&output=csv";
+const MATHSPHERE_DASHBOARD_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQH5a2TAZk-5N2Z-EK0ICk--y6lNA3wk-3GnvHTPND_IGmVwKABZmgtk07LH6K4RMsa-P7QTcHA_U3W/pub?gid=1616354092&single=true&output=csv";
 const MATHSPHERE_ADMIN_PASSCODE = "N3+w0rk5@)@^";
 
 function logResult(record){
